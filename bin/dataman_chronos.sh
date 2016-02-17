6cd00c66610adab8ea414abb6369221f4ae2a65e
Willbern <g.success16@gmail.com>
2016-02-17 10:31:16 +0800

    build a new chronos docker images

---
+++ bin/dataman_chronos.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+java -Djava.library.path=/usr/local/lib:/usr/lib:/usr/lib64 -Djava.util.logging.SimpleFormatter.format=%2$s%5$s%6$s%n $JAVA_OPTS -cp  mesosphere.marathon.Main --hostname $MARATHON_HOSTNAME --event_subscriber $SUBSCRIBER --master $MESOS_ZK --zk $MARATHON_ZK
+
